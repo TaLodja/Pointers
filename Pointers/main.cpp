19108e49925bc45b8e4ba89e40d79fd9fe2e73dd
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2025-04-12 15:01:38 +0500

    Add project files.

---
+++ Pointers/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello Pointers" << endl;
+
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;	//вывод переменной 'a' на экран
+	cout << &a << endl;	//взятие адреса переменной 'a' прямо при выводе
+	cout << pa << endl;	//вывод адреча переменной 'a', хранящегося в указателе 'pa'
+	cout << *pa << endl; //
+}
